d4bc29022156699d65cc6efc3c108c3bedf6ec24
Andy Jones <andytjones@icloud.com>
2019-11-13 21:54:57 +0000

    Added usage case tests. Refactored tests to DRY up test code.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,25 +8,46 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
 
-  it('new thermostat starts at 20', function(){
-    expect(thermostat.getCurrentTemperature()).toEqual(20);
-  });
+  describe('new thermostat', function(){
 
-  it('new thermostat min temp = 10', function(){
-    expect(thermostat.getMinTemperature()).toEqual(10);
-  });
+    it('does start temp at 20', function(){
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
 
+    it('has min temp 10', function(){
+      expect(thermostat.getMinTemperature()).toEqual(10);
+    }); 
+
+    it('has power saving mode on', function(){
+      expect(thermostat.isPowerSavingOn()).toBe(true);
+    });
+
+  });
 
   describe('changes temperature', function(){
 
-    it('increases by 1', function(){
+    it('does increase by 1', function(){
       thermostat.increaseTemperature();
       expect(thermostat.getCurrentTemperature()).toEqual(21);
+    });
+
+    it('does increase by 3', function(){
+      for (var i = 0; i < 3; i++){
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(23);
     });
 
     it('decreases by 1', function(){
       thermostat.decreaseTemperature();
       expect(thermostat.getCurrentTemperature()).toEqual(19);
+    });
+
+    it('decreases by 5', function(){
+      for (var i = 0; i < 5; i++){
+        thermostat.decreaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(15);
     });
 
   });
@@ -50,24 +71,21 @@ describe('Thermostat', function(){
 
   describe('power saving mode', function(){
 
-    it('has power saving mode on', function(){
-      expect(thermostat.isPowerSavingOn()).toBe(true);
+    beforeEach(function(){
+      thermostat.switchPowerSaving();
     });
 
     it('can switch power saving off', function(){
-      thermostat.switchPowerSaving();
       expect(thermostat.isPowerSavingOn()).toBe(false);
     });
 
     it('can switch power saving off then on', function(){
-      thermostat.switchPowerSaving();
       expect(thermostat.isPowerSavingOn()).toBe(false);
       thermostat.switchPowerSaving();
       expect(thermostat.isPowerSavingOn()).toBe(true);
     });
 
     it('is off and has MAX TEMP 32', function(){
-      thermostat.switchPowerSaving();
       expect(thermostat.isPowerSavingOn()).toBe(false);
       expect(thermostat.getMaxTemperature()).toBe(32);
     });
@@ -108,7 +126,31 @@ describe('Thermostat', function(){
       expect(thermostat.energyUsage()).toEqual('high-usage');
     });
 
+  });
 
+  describe('usage cases', function(){
+
+    beforeEach(function(){
+      for (var i = 0; i < 10; i++){
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25)
+      thermostat.switchPowerSaving();
+    });
+
+    it('increase temp, switch off PSM, carry on increasing', function(){
+      for (var i = 0; i < 5; i++){
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(30)
+    });
+
+    it('increase temp, switch off PSM, increase till MAX_TEMP', function(){
+      for (var i = 0; i < 15; i++){
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32)
+    });
 
   });
 
