0c3369b0b324e5385cfdeb4e6f75df7971d0ecfa
Andy Jones <andytjones@icloud.com>
2019-11-13 15:57:21 +0000

    Refactored code. Added getter and setter for temperatures.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,14 +1,19 @@
+'use strict';
+
 describe('Thermostat', function(){
+  
+  var thermostat;
+
   beforeEach(function(){
     thermostat = new Thermostat();
   });
 
   it('new thermostat starts at 20', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
   it('new thermostat min temp = 10', function(){
-    expect(thermostat.minimumTemperature).toEqual(10);
+    expect(thermostat.getMinTemp()).toEqual(10);
   });
 
 
@@ -16,23 +21,34 @@ describe('Thermostat', function(){
 
     it('increases by 1', function(){
       thermostat.increaseTemp();
-      expect(thermostat.temperature).toEqual(21);
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
     });
 
     it('decreases by 1', function(){
       thermostat.decreaseTemp();
-      expect(thermostat.temperature).toEqual(19);
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
     });
 
   });
 
   describe('power saving mode', function(){
 
-    it('is on, max temp should be 25', function(){
-      thermostat.temperature = 25;
-      thermostat.powerSavingMode = true;
-      expect( function() { thermostat.increaseTemp(); }).toThrow("Max temperature exceeded");
+    it('has power saving mode on', function(){
+      expect(thermostat.isPowerSavingOn()).toBe(true);
     });
+
+    it('can switch power saving off', function(){
+      thermostat.switchPowerSaving();
+      expect(thermostat.isPowerSavingOn()).toBe(false);
+    });
+
+    it('can switch power saving off then on', function(){
+      thermostat.switchPowerSaving();
+      expect(thermostat.isPowerSavingOn()).toBe(false);
+      thermostat.switchPowerSaving();
+      expect(thermostat.isPowerSavingOn()).toBe(true);
+    });
+    
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,16 +1,28 @@
 function Thermostat(){
   this.temperature = 20;
-  this.minimumTemperature = 10;
-  this.maximumTemperature = 25;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP = 25;
+  this.powerSaving = true;
+};
+
+Thermostat.prototype.getCurrentTemperature = function(){
+  return this.temperature;
+};
+
+Thermostat.prototype.getMinTemp = function(){
+  return this.MIN_TEMP;
+};
+
+Thermostat.prototype.isPowerSavingOn = function(){
+  return this.powerSaving;
+};
+
+Thermostat.prototype.switchPowerSaving = function(){
+  this.powerSaving = !this.powerSaving;
 };
 
 Thermostat.prototype.increaseTemp = function(){
-  if(this.maximumTemperature === this.temperature) {
-    throw "Max temperature exceeded";
-  } else {
     this.temperature ++;
-  }
-
 };
 
 Thermostat.prototype.decreaseTemp = function(){
